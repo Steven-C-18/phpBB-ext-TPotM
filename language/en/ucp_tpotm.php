354f6bed1c2d3dee4d0193a142cba2f65a22c095
S. McCandlish <smccandlish@gmail.com>
2025-01-19 05:58:36 -0800

    Update ucp_tpotm.php
    typographic cleanup

--- language/en/ucp_tpotm.php
+++ language/en/ucp_tpotm.php
@@ -20,7 +20,7 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, [
 	'USER_TOOLTIP_HOVER'		=> '<em><< tooltip</em>',
-	'USER_TOOLTIP'				=> 'Date format UTC+00:00 in Tooltips',
+	'USER_TOOLTIP'				=> 'Date format UTC+00:00 in tooltips',
 	'USER_TOOLTIP_EXPLAIN'		=> '<strong>Yes</strong> = Ex. <em>01 10 2017 00:01</em> to <em>01 11 2027 00:00</em><br /><strong>No</strong> = Your above date format',
 	'USER_TOOLTIP_SEL'			=> 'Display date format in tooltips',
 	'USER_TOOLTIP_SEL_EXPLAIN'	=> '<strong>No</strong> = Tooltips will be shown with a standard message ignoring the above selection',
