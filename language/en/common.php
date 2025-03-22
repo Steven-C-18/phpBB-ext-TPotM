c1b65923da31efc0a6b75ad24c3ffa71597bf8ae
S. McCandlish <smccandlish@gmail.com>
2025-03-21 19:03:21 -0700

    Update common.php
    Clarification; typographic cleanup.

--- language/en/common.php
+++ language/en/common.php
@@ -39,12 +39,12 @@ $lang = array_merge($lang, [
 	'TPOTM_NOW'						=> 'At the present time is ',
 	'TPOTM_NOBODY'					=> 'not yet available ',
 	'TPOTM_BADGE'					=> 'Badge ?',
-	'TPOTM_BADGE_MINIPROFILE'		=> 'Top poster of the Month',
+	'TPOTM_BADGE_MINIPROFILE'		=> 'Top Poster of the Month',
 	'TOTAL_MONTH'					=> ' out of <strong>%1s</strong> (%2s%%)',
 	'TPOTM_EXPLAIN'					=> 'From %1s to %2s',
 	'TPOTM_NO_EXPLAIN'				=> 'During the current month',
 	'TPOTM_TOT_POST'				=> 'Total posts',
-	'TPOTM_DATE'					=> 'Year and Month',
+	'TPOTM_DATE'					=> 'Year and month',
 	'TPOTM_LAST_POST_IN_MONTH'		=> 'Last on',
 
 	'TPOTM_CACHE'	=> [
@@ -59,20 +59,20 @@ $lang = array_merge($lang, [
 	],
 
 	// Translators please do not change the following line, no need to translate it!
-	'TPOTM_CREDIT_LINE'		=>	' | <a href="https://github.com/3D-I/tpotm">Top Poster of the Month</a> &copy; 2005, 2018 - 3Di',
+	'TPOTM_CREDIT_LINE'				=>	' | <a href="https://github.com/3D-I/tpotm">Top Poster of the Month</a> &copy; 2005, 2018 - 3Di',
 
-	// Hall of fame
-	'VIEWING_TPOTM_HALL'			=> 'Viewing TPOTM Hall of fame',
-	'TPOTM_PAGE'					=> 'Hall of fame',
-	'TPOTM_HELLO'					=> 'Top Poster of the Month - Hall Of Fame',
+	// Hall of Fame
+	'VIEWING_TPOTM_HALL'			=> 'Viewing TPotM Hall of Fame',
+	'TPOTM_PAGE'					=> 'Hall of Fame',
+	'TPOTM_HELLO'					=> 'Top Posters of the Month &ndash; Hall of Fame',
 	'TPOTM_EXPLAIN_HALL'			=> 'From %1s to %2s',
-	'TPOTM_HALL_NO_EXPLAIN'			=> 'Since Epoch till the very end of the previous month',
+	'TPOTM_HALL_NO_EXPLAIN'			=> 'Since epoch till the very end of the previous month',
 	'TPOTM_HALL_NO_TOP_POSTERS'		=> 'There are no past Top Posters yet to display.',
 
 	'HALL_OF_FAME'	=> [
-		0	=> 'Hall of fame',
-		1	=> 'Hall of fame &bull; page %d',
-		2	=> 'Hall of fame &bull; page %d',
+		0	=> 'Hall of Fame',
+		1	=> 'Hall of Fame &bull; page %d',
+		2	=> 'Hall of Fame &bull; page %d',
 	],
 
 	'TPOTM_HALL_COUNT'	=> [
@@ -80,6 +80,6 @@ $lang = array_merge($lang, [
 		2	=> ' Found a total of <strong>%d</strong> top posters',
 	],
 
-	'NOT_AUTHORISED_TPOTM_HALL'		=> 'You are not authorized to see the Hall of fame of Top Poster of the Month extension.',
-	'TPOTM_HALL_DISABLED'			=> 'The Hall of fame of Top Poster of the Month extension is momentanely disabled.',
+	'NOT_AUTHORISED_TPOTM_HALL'		=> 'You are not authorized to see the Top Posters of the Month Hall of Fame.',
+	'TPOTM_HALL_DISABLED'			=> 'The Top Posters of the Month Hall of Fame is disabled.',
 ]);
